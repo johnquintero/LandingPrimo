51163da02a09fbc6bf1d4742b26e830bd3a8996b
johnkintero <johnjairoqp@gmail.com>
2020-06-24 20:47:13 -0500

    Se ajusto el problema del la libreria body-parser y las rutas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
--- routes/index.js
+++ routes/index.js
@@ -4,108 +4,13 @@ const https = require('https');
 const fetch = require('node-fetch');
 
 
-router.get('/',(req,res) => {
+router.get('*',(req,res) => {
     res.render('index');
     //res.sendfile(path.join(__dirname, '/views/index.html'));
     //console.log(__dirname)
 });
 
-router.post('/new',(req,res) => {
-    res.json({requestBody: req.body}); 
-    console.log(req.body);
-    var razonSocial = req.body.NomEmpresa;
-    var dirEmpresa = req.body.DirEmpresa;
-    var ciudad = req.body.Ciudad;
-    var sucursalSi = req.body.gridRadios;
-    var contacto = req.body.Contacto;
-    var celular = req.body.Celular;
-    var anno = req.body.Annos;
-    var email = req.body.Email;
-    var iServicio;
-    if(!req.body.iServicioGeneral){
-        iServicio = "N";
-    }
-    else {
-        iServicio = "S";
-    }
-    var iLatoneria;
-    if(!req.body.iLatoneria){
-        iLatoneria = "N";
-    }
-    else {
-        iLatoneria = "S";
-    }
-    var iRevision = req.body.iRevision == undefined ? "N" : "S";
-    var iReparaciones = req.body.iReparaciones == undefined ? "N" : "S";
-    var iReparaMayores = req.body.IReparacionesMayores == undefined ? "N" : "S";
-    var iCambioLlantas = req.body.iCambioLlano == undefined ? "N" : "S";
-    var iRectificacion = req.body.iRectificacion == undefined ? "N" : "S";
-    var iPersonalizacion = req.body.iPersonalizacion == undefined ? "N" : "S";
-    var iLavado = req.body.iLavado == undefined ? "N" : "S";
-    var iGrua = req.body.iGrua == undefined ? "N" : "S";
-    var iElectricidad = req.body.iElectricidad == undefined ? "N" : "S";
-    var iOtros = req.body.iOtros == undefined ? "N" : "S";
-
-    const data = JSON.stringify({
-        razon : razonSocial,
-        direcc : dirEmpresa,
-        contacto : contacto,
-        mail : email,
-        tele : celular,
-        ciudad : ciudad,
-        annos : anno,
-        sucursal : sucursalSi,
-        servicio : iServicio,
-        latoneria : iLatoneria,
-        revision : iRevision,
-        reparmenor : iReparaciones,
-        reparmayor : iReparaMayores,
-        cambio : iCambioLlantas,
-        rectificacion : iRectificacion,
-        personalizacion :iPersonalizacion,
-        lavado : iLavado,
-        grua : iGrua,
-        electrico : iElectricidad,
-        otros : iOtros
-    });
-
-    // prepare the header
-    var postheaders = {
-        'Content-Type' : 'application/json',
-        'Content-Length' : Buffer.byteLength(data, 'utf8')
-    };
-
-    // the post options
-    var optionspost = {
-        host : 'primo.com.co/Service',
-        port : 443,
-        path : '/',
-        method : 'POST',
-        headers : postheaders
-    };
-
-    var reqPost = https.request(optionspost, function(res) {
-        console.log("statusCode: ", res.statusCode);
-        // uncomment it for header details
-    //  console.log("headers: ", res.headers);
-    
-        res.on('data', function(d) {
-            console.info('POST result:\n');
-            process.stdout.write(d);
-            console.info('\n\nPOST completed');
-        });
-    });
-
-    // write the json data
-    reqPost.write(data);
-    reqPost.end();
-    reqPost.on('error', function(e) {
-        console.error(e);
-    });
-});
-
-
-router.post('/new2',(req,res) => { 
+router.post('*new2',(req,res) => { 
     //res.json({requestBody: req.body}); 
     //console.log(req.body);
     var razonSocial = req.body.NomEmpresa;
@@ -116,30 +21,32 @@ router.post('/new2',(req,res) => {
     var celular = req.body.Celular;
     var anno = req.body.Annos;
     var email = req.body.Email;
-    var iServicio;
-    if(!req.body.iServicioGeneral){
-        iServicio = "N";
-    }
-    else {
-        iServicio = "S";
-    }
-    var iLatoneria;
-    if(!req.body.iLatoneria){
-        iLatoneria = "N";
-    }
-    else {
-        iLatoneria = "S";
-    }
-    var iRevision = req.body.iRevision == undefined ? "N" : "S";
-    var iReparaciones = req.body.iReparaciones == undefined ? "N" : "S";
-    var iReparaMayores = req.body.IReparacionesMayores == undefined ? "N" : "S";
-    var iCambioLlantas = req.body.iCambioLlano == undefined ? "N" : "S";
-    var iRectificacion = req.body.iRectificacion == undefined ? "N" : "S";
-    var iPersonalizacion = req.body.iPersonalizacion == undefined ? "N" : "S";
-    var iLavado = req.body.iLavado == undefined ? "N" : "S";
-    var iGrua = req.body.iGrua == undefined ? "N" : "S";
+    var localidad = req.body.Localidad;
+    var marca = req.body.marca == "0" ? req.body.otramarca : req.body.marca;
+    var otramarca = req.body.otramarca == undefined ? "" : req.body.otramarca;
+    var iAlineacion = req.body.iAlineacion == undefined ? "N" : "S";
+    var iCambioBateria = req.body.iCambioBateria == undefined ? "N" : "S";
+    var iCambioCorrea = req.body.iCambioCorrea == undefined ? "N" : "S";
     var iElectricidad = req.body.iElectricidad == undefined ? "N" : "S";
-    var iOtros = req.body.iOtros == undefined ? "N" : "S";
+    var iInstalacion = req.body.iInstalacion == undefined ? "N" : "S";
+    var iPolarizado = req.body.iPolarizado == undefined ? "N" : "S";
+    var iRecubrimiento = req.body.iRecubrimiento == undefined ? "N" : "S";
+    var iReparacion = req.body.iReparacion == undefined ? "N" : "S";
+    var iRevisionPre = req.body.iRevisionPre == undefined ? "N" : "S";
+    var iGrua = req.body.iGrua == undefined ? "N" : "S";
+    var iSuspension = req.body.iSuspension == undefined ? "N" : "S";
+    var iOtro = req.body.iOtro == undefined ? "N" : req.body.otros;
+    var iCambioAceite = req.body.iCambioAceite == undefined ? "N" : "S";
+    var iCambioAceiteDom = req.body.iCambioAceiteDom == undefined ? "N" : "S";
+    var iCambioLlantas = req.body.iCambioLlantas == undefined ? "N" : "S";
+    var iElevavidrios = req.body.iElevavidrios == undefined ? "N" : "S";
+    var iLatoneria = req.body.iLatoneria == undefined ? "N" : "S";
+    var iLatoneriaPdr = req.body.iLatoneriaPdr == undefined ? "N" : "S";
+    var iRevision = req.body.iRevision == undefined ? "N" : "S";
+    var iTapiceria = req.body.iTapiceria == undefined ? "N" : "S";
+    var iTapiceriaLava = req.body.iTapiceriaLava == undefined ? "N" : "S";
+    var iVentaLlantas = req.body.iVentaLlantas == undefined ? "N" : "S";
+    var iVentaLujos = req.body.iVentaLujos == undefined ? "N" : "S";
 
     const data = JSON.stringify({
         razon : razonSocial,
@@ -150,21 +57,35 @@ router.post('/new2',(req,res) => {
         ciudad : ciudad,
         annos : anno,
         sucursal : sucursalSi,
-        servicio : iServicio,
-        latoneria : iLatoneria,
-        revision : iRevision,
-        reparmenor : iReparaciones,
-        reparmayor : iReparaMayores,
-        cambio : iCambioLlantas,
-        rectificacion : iRectificacion,
-        personalizacion :iPersonalizacion,
-        lavado : iLavado,
+        alineacion : iAlineacion,
+        bateria : iCambioBateria,
+        correa : iCambioCorrea,
+        electricidad : iElectricidad,
+        instalacion : iInstalacion,
+        polarizado : iPolarizado,
+        recubrimiento : iRecubrimiento,
+        reparacion :iReparacion,
+        revision : iRevisionPre,
         grua : iGrua,
-        electrico : iElectricidad,
-        otros : iOtros
+        suspension : iSuspension,
+        otros : iOtro,
+        cambio : iCambioAceite,
+        cambiodom : iCambioAceiteDom,
+        llanta : iCambioLlantas,
+        elevavidrios : iElevavidrios,
+        latoneria : iLatoneria,
+        pdr : iLatoneriaPdr,
+        frenos : iRevision,
+        tapiceria : iTapiceria,
+        tapicerialava : iTapiceriaLava,
+        ventallanta : iVentaLlantas,
+        ventalujo : iVentaLujos,
+        localidad : localidad,
+        marca : marca
+
     });
 
-
+/* fetch("http://localhost:3000",{*/
     fetch("http://www.primo.com.co/Servicio",{
         method: 'POST',
         body : data,
